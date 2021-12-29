8fed3648952653ef3537b500b3a7a22d8ad7b194
PiotrKozlowski11 <85372826+PiotrKozlowski11@users.noreply.github.com>
2021-12-29 19:43:07 +0100

    Added CategoryController and test.

---
+++ src/main/java/org/kozlowski/fruitshop/controllers/CategoryController.java
@@ -0,0 +1,38 @@
+package org.kozlowski.fruitshop.controllers;
+
+import org.kozlowski.fruitshop.api.model.CategoryDTO;
+import org.kozlowski.fruitshop.api.model.CategoryListDTO;
+import org.kozlowski.fruitshop.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/categories/")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListDTO> getAllCategories() {
+
+        return new ResponseEntity<>(
+                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK
+        );
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+
+        return new ResponseEntity<>(
+                categoryService.getCategoryByName(name), HttpStatus.OK
+        );
+
+    }
+}
